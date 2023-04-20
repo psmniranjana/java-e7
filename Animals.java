5cc7a19a60c6665428b5b7323a96581cd3ab5918
Maleesha Niranjana <117069472+psmniranjana@users.noreply.github.com>
2023-04-20 12:01:37 +0530

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animals;
+
+
+public class Animals {
+
+    public static void main(String[] args) {
+         dog d = new dog();
+        d.eat();
+    }
+    }
+    
+
